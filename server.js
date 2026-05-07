6fa10441720108aeadd1ac84ddffe665a630888e
GarGetDev73 <hussam.github@gmail.com>
2026-05-07 11:25:54 +0300

    fix-smtp-rep

--- server.js
+++ server.js
@@ -34,7 +34,9 @@ module.exports = User;
 const storage = multer.memoryStorage();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
